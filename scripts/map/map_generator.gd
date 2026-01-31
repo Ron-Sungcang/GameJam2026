5ef319fa74a86746ef353243347b0c443e00d129
Ron Sungcang <rts966@usask.ca>
2026-01-31 14:42:48 -0500

    Started implementing pathing

--- scripts/map/map_generator.gd
+++ scripts/map/map_generator.gd
@@ -28,8 +28,18 @@ func generate_map() -> Array[Array]:
 	map_array = _generate_initial_grid()
 	var starting_point := _get_starting_points()
 	
-	print(starting_point)
-	
+	for j in starting_point:
+		var current_j := j
+		for i in FLOORS - 1:
+			current_j = _setup_connection(i, current_j)
+	var i := 0
+	for floor in map_array:
+		print("floor %s" % i)
+		var used_icons = floor.filter(
+			func(room: MapIconInfo): return room.next_icons.size() > 0
+		)
+		print(used_icons)
+		i += 1
 	return []
 
 func _generate_initial_grid() -> Array[Array]:
@@ -67,3 +77,33 @@ func _get_starting_points() -> Array[int]:
 			y_coord.append(starting_point)
 	return y_coord
 		
+func _setup_connection(i: int, j: int) -> int:
+	var next_icon: MapIconInfo
+	var current_icon := map_array[i][j] as MapIconInfo
+	
+	while not next_icon or _would_cross_existing_path(i, j, next_icon):
+		var random_j := clampi(randi_range(j - 1, j + 1), 0, MAP_WIDTH - 1)
+		next_icon = map_array[i+1][random_j]
+	current_icon.next_icons.append(next_icon)
+	return next_icon.column
+	
+func _would_cross_existing_path(i: int, j: int, next: MapIconInfo) -> bool:
+	var left_neighbour: MapIconInfo
+	var right_neighbour: MapIconInfo
+	
+	if j > 0:
+		left_neighbour = map_array[i][j-1]
+	if j < MAP_WIDTH - 1:
+		right_neighbour = map_array[i][j+1]
+	
+	if right_neighbour and next.column > j:
+		for next_icon: MapIconInfo in right_neighbour.next_icons:
+			if next_icon.column < next.column:
+				return true
+				
+	if left_neighbour and next.column < j:
+		for next_icon: MapIconInfo in left_neighbour.next_icons:
+			if next_icon.column > next.column:
+				return true
+	
+	return false
