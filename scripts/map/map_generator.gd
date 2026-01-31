8a2075c4bc139b51dafb40addeb8b7ecc8082a4b
Ron Sungcang <rts966@usask.ca>
2026-01-31 13:58:22 -0500

    Floors are getting generated now

--- scripts/map/map_generator.gd
+++ scripts/map/map_generator.gd
@@ -18,5 +18,34 @@ var random_room_weights = {
 	MapIconInfo.Type.ELITE: 0.0
 }
 
+func _ready() -> void:
+	generate_map()
+
 var random_room_type_total_weight := 0
 var map_array: Array[Array]
+
+func generate_map() -> Array[Array]:
+	map_array = _generate_initial_grid()
+	
+	return []
+
+func _generate_initial_grid():
+	var result: Array[Array] = []
+	for i in FLOORS:
+		var adjacent_rooms: Array[MapIconInfo] = []
+		
+		for j in MAP_WIDTH:
+			var current_icon := MapIconInfo.new()
+			var offset := Vector2(randf(), randf()) * RANDOMNESS
+			current_icon.position = Vector2(j* X_DIST, i * -Y_DIST) + offset
+			current_icon.row = i
+			current_icon.column = j
+			current_icon.next_icons = []
+			
+			if i == FLOORS - 1:
+				current_icon.position.y = (i+1) * -Y_DIST
+				
+			adjacent_rooms.append(current_icon)
+			
+		result.append(adjacent_rooms)
+	return result
